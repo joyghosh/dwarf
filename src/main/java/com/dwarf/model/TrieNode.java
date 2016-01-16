fc7160bc77dcbee0f06970e97c518cce3386427a
joyghosh <joy.ghosh@tiatros.com>
2016-01-16 21:40:32 +0530

    [master] distributed start point for scalability

--- src/main/java/com/dwarf/model/TrieNode.java
+++ src/main/java/com/dwarf/model/TrieNode.java
@@ -8,6 +8,8 @@ package com.dwarf.model;
 public abstract class TrieNode{
 	
 	protected boolean isLeaf;
+	protected int level;
+	protected String serverId;			//for horizontal partitioning.
 	
 	protected TrieNode(){
 		this.isLeaf = false;
@@ -27,5 +29,37 @@ public abstract class TrieNode{
 	 */
 	public void setLeaf(boolean isLeaf) {
 		this.isLeaf = isLeaf;
+	}
+	
+	/**
+	 * fetch the level of the node.
+	 * @return
+	 */
+	public int getLevel(){
+		return this.level;
+	}
+	
+	/**
+	 * set the level of the node.
+	 * @param level
+	 */
+	public void setLevel(int level){
+		this.level = level;
+	}
+	
+	/**
+	 * fetch server id for distributed look-up.
+	 * @return the server id to look for next node.
+	 */
+	public String getServerId(){
+		return this.serverId;
+	}
+	
+	/**
+	 * set the server id to look-up for this node.
+	 * @param serverId
+	 */
+	public void setServerId(String serverId){
+		this.serverId = serverId;
 	}
 }
